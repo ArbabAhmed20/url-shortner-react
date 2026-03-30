6491bce619076bb91703e6e7a25537929d670105
Arbab Ahmed <arbabahmed631@gmail.com>
2026-03-30 19:35:43 +0530

    Url

--- src/utills/Constant.js
+++ src/utills/Constant.js
@@ -1,7 +1,7 @@
 import AppRouter, {SubDomainRouter} from "../AppRouter.jsx";
 
 export const subDomainList = [
-    {subdomain: "www", app: AppRouter, main: true},
+    {subdomain: "", app: AppRouter, main: true},
     {subdomain: "urlshortnr", app: SubDomainRouter, main: false},
     {}
 ];
