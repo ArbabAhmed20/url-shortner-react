89599e4b9ce141e43c305acd26c835d028631e8f
Arbab Ahmed <arbabahmed631@gmail.com>
2026-03-30 21:23:03 +0530

    "fixed url redirection"

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,12 +1,15 @@
 import NavBar from "./components/NavBar.jsx";
 import {Toaster} from "react-hot-toast";
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import LandingPage from "./components/LandingPage.jsx";
 import AboutPage from "./components/AboutPage.jsx";
 import RegisterPage from "./components/RegisterPage.jsx";
 import Login from "./components/Login.jsx";
 import DashboardLayout from "./dashboard/DashboardLayout.jsx";
 import Footer from "./components/Footer.jsx";
+import ShortenURlPage from "./components/ShortenURlPage.jsx";
+import PrivateRoute from "./PrivateRoute.jsx";
+import ErrorPage from "./components/ErrorPage.jsx";
 
 const AppRouter = () => {
     return (
@@ -19,18 +22,14 @@ const AppRouter = () => {
                 <Route path="/register" element={<PrivateRoute publicPage={true}><RegisterPage /></PrivateRoute>} />
                 <Route path="/login" element={<PrivateRoute publicPage={true}><Login /></PrivateRoute>} />
                 <Route path="/dashboard" element={<PrivateRoute publicPage={false}><DashboardLayout /></PrivateRoute>} />
-                <Route path="/*" element={<ErrorPage message={"Page Not Found!"} />} />
                 <Route path="/error" element={<ErrorPage />} />
+                <Route path="/:url" element={<ShortenURlPage />} />
+                <Route path="/*" element={<ErrorPage message={"Page Not Found!"} />} />
             </Routes>
             <Footer/>
         </>
     )
 }
-
-import React from 'react'
-import ShortenURlPage from "./components/ShortenURlPage.jsx";
-import PrivateRoute from "./PrivateRoute.jsx";
-import ErrorPage from "./components/ErrorPage.jsx";
 
 export const SubDomainRouter = () => {
     return (
